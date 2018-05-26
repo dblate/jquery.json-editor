138ee42d2a11d4aad63df6eb8bd24670ec133227
yuhui06 <yuhui06@baidu.com>
2018-05-26 18:39:03 +0800

    增加 debug 模式

--- fis-conf.js
+++ fis-conf.js
@@ -11,9 +11,11 @@ fis.match('**', {release: false});
 
 fis.match('src/index.js', {
     optimizer: fis.plugin('uglify-js'),
-    preprocessor: fis.plugin('js-require-css', {
-        mode: 'inline'
-    }),
+    preprocessor: fis.plugin('js-require-css', {mode: 'inline'}),
     release: fis.get('file-name') + '.min.js'
+});
+
+fis.media('debug').match('src/index.js', {
+    optimizer: null
 });
 
